86a4adbd1036630c9795be66e0eacbf172566ac6
BobrZla <BobrZla@gmail.com>
2026-02-09 22:37:01 +0700

    Считываем все csv файлы и делаем из них список словарей для удобной работы с данными.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Здесь основной код скрипта
 import argparse
+import csv
 import sys
 
 
@@ -24,6 +25,27 @@ def main():
         help='Название отчета для создания (например: "average-gdp").',
     )
     args = parser.parse_args()
+    all_data = [] # Тут будем хранить данные из csv файлов.
+
+    for file_path in args.files:
+        try:
+            with open(file_path, mode='r', encoding='utf-8') as f:
+                # csv.DictReader позволяет читать строки как словари, где ключи - это заголовки
+                reader = csv.DictReader(f)
+                for row in reader:
+                    # Каждая строка - это словарь {'country': 'USA', 'year': 1992}
+                    all_data.append(row)
+        except FileNotFoundError:
+            print(f'Ошибка: Файл не найден по пути - {file_path}', file=sys.stderr)
+            sys.exit(1)
+        except Exception as e:
+            print('Ошибка чтения файла {file_path}: {e}', file=sys.stderr)
+            sys.exit(1)
+
+    print(f'Успешно загруженны {len(all_data)} данных из {len(args.files)} файлов.')
+
+    if all_data:
+        print("Первая запись:", all_data[0])
 
     # Тестовый принт
     print(f"Файлы для обработки: {args.files}")
